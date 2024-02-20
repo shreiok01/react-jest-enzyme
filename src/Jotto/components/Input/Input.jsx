ec8199d3e3b91a47901f59862e99c9b4e50de5e2
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-02-20 17:16:40 +0530

    write checkProps tests for input component :sparkles:

--- src/Jotto/components/Input/Input.jsx
+++ src/Jotto/components/Input/Input.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Input = (props) => {
+const Input = ({secretWord}) => {
   return (
     <div data-test="component-input">Input</div>
   )
+}
+
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired
 }
 
 export default Input
--- src/Jotto/components/Input/Input.test.js
+++ src/Jotto/components/Input/Input.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Input from "./Input";
+import { checkProps, findByTestAttribute } from "../../../../tests/utils";
 
 /**
  * Factory function to create Shallow Wrapper for the Input Component.
@@ -8,16 +9,17 @@ import Input from "./Input";
  * @returns {ShallowWrapper}
  */
 
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = "party" ) => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
-const findByTestAttribute = (wrapper, value) =>
-  wrapper.find(`[data-test='${value}']`);
 
-  test("render without errors", () => {
-    const wrapper = setup();
-    const CounterComponent = findByTestAttribute(wrapper, "component-input");
-    expect(CounterComponent.length).toBe(1);
-  });
-  
+test("render without errors", () => {
+  const wrapper = setup();
+  const InputComponent = findByTestAttribute(wrapper, "component-input");
+  expect(InputComponent.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
+});
