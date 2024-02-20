45e264f2081e0f728124d72d944ad52ffa6d7ff3
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-02-20 11:19:01 +0530

    Test and code for non-zero guessed words

--- src/Jotto/components/GuessedWords.test.js
+++ src/Jotto/components/GuessedWords.test.js
@@ -18,22 +18,48 @@ const setup = (props = {}) => {
   return shallow(<GuessedWords {...setupProps} />);
 };
 
-test('does not throw warning with expected props', () => {
+test("does not throw warning with expected props", () => {
   checkProps(GuessedWords, defaultProps);
 });
 
-describe('if there are no words guessed', () => {
-  let wrapper
+describe("if there are no words guessed", () => {
+  let wrapper;
   beforeEach(() => {
     wrapper = setup({ guessedWords: [] });
   });
-  test('renders without error', () => {
-    const component = findByTestAttribute(wrapper, 'component-guessed-words');
+  test("renders without error", () => {
+    const component = findByTestAttribute(wrapper, "component-guessed-words");
     expect(component.length).toBe(1);
   });
-  test('renders instructions to guess a word', () => {
-    const instructions = findByTestAttribute(wrapper, 'guess-instructions');
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttribute(wrapper, "guess-instructions");
     expect(instructions.text().length).not.toBe(0);
   });
 });
 
+describe("if there are words guesses", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test("renders without errors", () => {
+    const component = findByTestAttribute(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders 'guessed words' section", () => {
+    const guessedWordsNodes = findByTestAttribute(wrapper, "guessed-words");
+    expect(guessedWordsNodes.length).toBe(1);
+  });
+
+  test("correct number of guessed words", () => {
+    const guessedWordNodes = findByTestAttribute(wrapper, "guessed-word");
+    expect(guessedWordNodes.length).toBe(guessedWords.length)
+  });
+});
