afbee99d930403590aebb2ac1a71fc48bd971a7e
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-02-21 16:04:46 +0530

    Testing input render for 'success' as 'true' or 'false' :sparkles:

--- src/Jotto/components/Input/Input.jsx
+++ src/Jotto/components/Input/Input.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Input = ({ secretWord }) => {
+const Input = ({ success, secretWord }) => {
   const [currentGuess, setCurrentGuess] = React.useState("");
+
+  if (success) {
+    return <div data-test="component-input"/>
+  }
   return (
     <div data-test="component-input">
       <form action="" className="form-inline">
--- src/Jotto/components/Input/Input.test.js
+++ src/Jotto/components/Input/Input.test.js
@@ -9,14 +9,51 @@ import { checkProps, findByTestAttribute } from "../../../../tests/utils";
  * @returns {ShallowWrapper}
  */
 
-const setup = (secretWord = "party") => {
-  return shallow(<Input secretWord={secretWord} />);
+const setup = (success = false, secretWord = "party") => {
+  return shallow(<Input success={success} secretWord={secretWord} />);
 };
 
-test("render without errors", () => {
-  const wrapper = setup();
-  const InputComponent = findByTestAttribute(wrapper, "component-input");
-  expect(InputComponent.length).toBe(1);
+describe("renders", () => {
+  describe("success is true", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup(true);
+    });
+    test("render without errors", () => {
+      const InputComponent = findByTestAttribute(wrapper, "component-input");
+      expect(InputComponent.length).toBe(1);
+    });
+    test("input box does not show", () => {
+      const InputBox = findByTestAttribute(wrapper, "input-box");
+      expect(InputBox.exists()).toBe(false);
+    });
+    test("submit button does not show", () => {
+      const submitButton = findByTestAttribute(wrapper, "submit-button");
+      expect(submitButton.exists()).toBe(false);
+    });
+  });
+
+  describe("success is false", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup(false);
+    });
+
+    test("render without errors", () => {
+      const InputComponent = findByTestAttribute(wrapper, "component-input");
+      expect(InputComponent.length).toBe(1);
+    });
+
+    test("input box shows", () => {
+      const InputBox = findByTestAttribute(wrapper, "input-box");
+      expect(InputBox.exists()).toBe(true);
+    });
+
+    test("submit button shows", () => {
+      const submitButton = findByTestAttribute(wrapper, "submit-button");
+      expect(submitButton.exists()).toBe(true);
+    });
+  });
 });
 
 test("does not throw warning with expected props", () => {
@@ -51,7 +88,7 @@ describe("state controlled input field", () => {
   test("field is cleared upon submit button click", () => {
     const submitButton = findByTestAttribute(wrapper, "submit-button");
 
-    submitButton.simulate("click", { preventDefault() {}});
+    submitButton.simulate("click", { preventDefault() {} });
     expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
   });
 });
