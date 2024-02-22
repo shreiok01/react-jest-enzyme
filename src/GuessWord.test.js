fcae9fc7baa33599048c0fb2490c1e3986e01b69
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2024-02-22 11:39:55 +0530

    write some words guesses and correct word guesses failing tests :zap:

--- src/GuessWord.test.js
+++ src/GuessWord.test.js
@@ -44,4 +44,53 @@ describe("No words guessed", () => {
   });
 });
 
-describe("Some words guessed", () => {});
+describe("Some words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWord: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+  });
+
+  test("adds row to guessed word table", () => {
+    const guessedWordNodes = findByTestAttribute(wrapper, "guessed-word");
+    expect(guessedWordNodes).toHaveLength(2);
+  });
+});
+
+describe("guess secret word", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWord: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+    const InputBox = findByTestAttribute(wrapper, "input-box");
+    const mockEvt = { target: { value: "party" } };
+    InputBox.simulate("change", mockEvt);
+
+    const SubmitButton = findByTestAttribute("submit-button");
+    SubmitButton.simulate("click", { preventDefault() {} });
+  });
+
+  test("adds row to guessed word table", () => {
+    const guessedWordNodes = findByTestAttribute(wrapper, "guessed-word");
+    expect(guessedWordNodes).toHaveLength(3);
+  });
+
+  test("display congrats component", () => {
+    const congrats = findByTestAttribute(wrapper, "component-congrats");
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+
+  test("does not displays input component contents", () => {
+    const InputComponentContents = findByTestAttribute("input-box");
+    expect(InputComponentContents.exists()).toBe(false);
+
+    const SubmitButton = findByTestAttribute(wrapper, "submit-button");
+    expect(SubmitButton.exists()).toBe(false);
+  });
+});
